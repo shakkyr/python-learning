08c159e30f2196bb00adf45794cb1ae22a950cd1
Shadi Rada <shakkyr@gmail.com>
2022-03-08 00:47:20 +0200

    5 commit

---
+++ 06-Escape_sequence_Characters.py
@@ -0,0 +1,35 @@
+# \b => back space
+# \newline => escape new line + \
+# \\ => Escape BACK
+# \' => Escape Single Quotes
+# \" => Escape Double Quotes
+# \n => line feed
+# \r => carriage return
+# \t => Horizontal Tab
+# \xhh => Hex Value
+
+# back space
+print('hello\bshadi') # will remove the o
+
+# newline
+print("hello \
+    shadi\
+        in python")
+
+# Escape back Slash 
+print('i love you\\')
+
+# Escape Single + double quote
+print('i love \'shadi\' ' )
+
+# Line Feed
+print('hello \n world')
+
+# Carriage Return
+print('123456\rWorld') #replaces characters
+
+# Horizontal Tap
+print('hello\tworld')
+
+# Hex value
+print('\x4f\x73')
