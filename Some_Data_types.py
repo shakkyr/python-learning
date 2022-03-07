010ef6586ee59093a1a6de773d3493be12bb4bfe
Shadi Rada <shakkyr@gmail.com>
2022-03-07 23:57:28 +0200

    3 commit

---
+++ Some_Data_types.py
@@ -0,0 +1,32 @@
+# ---------------------
+# type()
+# All Data in python is objects
+# --------------------
+print(type(10)) #integet
+print(type(-10)) #integet
+
+
+print(type(9.8)) #float => Floating Point NUmber
+
+
+print(type("9.8")) #str => String
+print(type("Hello Shadi")) #str => String
+
+
+print(type([])) #list 
+print(type( [1, 3, 5, 6] )) #list 
+
+
+print(type( () )) #tuple 
+print(type( (1, 3, 5, 6) )) #tuple 
+
+
+print(type( {} )) #dict => dictionary
+print(type( {"one": 1, "Tow": 2, "Three": 3} )) #dict => dictionary
+
+
+print(type( 2 == 2 )) #bool => Boolean (True)
+print(type( 2 == 4 )) #bool => Boolean (False)
+
+
+
---
+++ Variables.py
@@ -0,0 +1,14 @@
+# ---------------------
+# -- Variables ==
+# --------------------
+# Syntax => [Variable Name] [Assigment Operator] [Value]
+
+# Name Convention and Rules
+# [1] can start wit (a-z A-Z) or Underscore
+# [2]
+# [3]
+# [4]
+# [5]
+# ---------------------
+
+myVariable = "My Value"
