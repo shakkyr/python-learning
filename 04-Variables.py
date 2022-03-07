8cf61dd10af3f81a07397d04d9715afec4798733
Shadi Rada <shakkyr@gmail.com>
2022-03-08 00:30:00 +0200

    4 commit

---
+++ 04-Variables.py
@@ -0,0 +1,19 @@
+# ---------------------
+# -- Variables ==
+# --------------------
+# Syntax => [Variable Name] [Assigment Operator] [Value]
+
+# Name Convention and Rules
+# [1] can start wit (a-z A-Z) or Underscore
+# [2] can't start with numbers Or Special Characters
+# [3] can include Numbers Inline
+# [4] can't Include Special Characters inLine
+# [5] Case Sensitive
+# ---------------------
+
+myVariable = "My Value"
+
+name = 'Shadi Rada' #Single word Decliration
+myName = 'Shadi Rada' #CameCase Decliration
+my_name = 'Shadi Rada' #snake_case Decliration
+
---
+++ 05-Variables_2.py
@@ -0,0 +1,20 @@
+# ---------------------
+# -- Variables ==
+# --------------------
+
+
+# Source Code : Orginal Code You Write On Computer
+# Translation : Converting Code Into Machine language
+# Compilation : Translate Code Before Run Time
+# Run Time : the Period App takes To Excute Commands
+# Interpreted : Code Translate On The Fly During Execution
+# ---------------------
+x = 10
+x = 'Hello'
+print(x)
+
+help("keywords") # prints the Reserved words for python
+
+
+a, b , c = 1, 2 ,4 
+
