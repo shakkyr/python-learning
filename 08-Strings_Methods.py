5dffe17417040825f552dea6084fc4ad4d9a3d84
Shadi Rada <shakkyr@gmail.com>
2022-03-13 13:22:29 +0200

    5 commit

--- 08-Strings_Methods.py
+++ 08-Strings_Methods.py
@@ -67,5 +67,72 @@ print(a.count(10))
 
 # swapcase()
 
-b = 'shadi is learning python and react native and'
+b = 'ShadI Is lEaRniNg pYtHon aNd rEacT'
+print(b.swapcase())
 
+# startswith() endswith()
+
+b = 'ShadI Is lEaRniNg pYtHon aNd rEacT'
+print(b.startswith('Is'))
+print(b.startswith('Is',6,20))
+
+# index(subString, Start, End)
+print(b.index('I')) # index 4
+print(b.index('I',5,40)) # index 6
+# print(b.index('I',10,20)) # not found returns error stops the code
+
+# find(subString, Start, End)
+
+print(b.find('I')) # index 4
+print(b.find('I',5,40)) # index 6
+print(b.find('I',10,20)) # -1 it's not found
+
+# rjust() ljust() adds spaces or charatcters befor/ after the string
+n = "shadi"
+print(n.rjust(10,"@"))
+print(n.ljust(10,"2"))
+
+# splitlines() returns a list from the string's lines 
+
+e = """ First Line
+Second line
+Third Line
+"""
+r = "First Line \n Second Line \n Third Line"
+
+print(e.splitlines())
+print(r.splitlines())
+
+# expandtabs() controll the added tabs
+
+b = 'ShadI\tIs\tlEaRniNg\tpYtHon\taNd\trEacT' #false
+c= 'Shadi Rada ' #true
+d= 'Shadi rada ' #false
+e= 'sjadi rada'
+
+print(b.expandtabs(5))
+
+# istitle()  isspace()
+
+print(b.istitle())
+print(c.istitle())
+print(d.istitle())
+print(e.isspace())
+
+# isidentifier()
+one = "ShadiRada"
+two = "shad_Rada"
+three = "shadi-Rada" # cant be as an identifier
+
+print(one.isidentifier())
+print(two.isidentifier())
+print(three.isidentifier())
+
+
+# isalpha() checks if the string is alpha petaal
+# isalnum() checks if string contains both numbers and characters
+
+v = 'asdasda'
+b = 'safa2324'
+print(v.isalpha()) # true
+print(b.isalpha()) # false
