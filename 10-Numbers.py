80f35344f9628b0cf97d7f9e310cba8bef679eb0
Shadi Rada <shakkyr@gmail.com>
2022-03-16 15:36:13 +0200

    7 commit

---
+++ 10-Numbers.py
@@ -0,0 +1,14 @@
+# lists
+
+myArr = ["One", "Two", "One", 1, 100.3, True]
+
+print(myArr[-1])  # last item
+
+print(myArr[1:5]) # 'Two', 'One', 1, 100.3  slice
+
+print(myArr[::2])  # ['One', 'One', 100.3]
+
+myArr[0:3] = []  # cleans the first 3 elements of the list
+
+print(myArr)
+
